1730f07211058f13c6535db59ade7b25c4c931ae
Kaushal Sharma <your-shkaushal-email@example.com>
2025-07-04 08:49:30 +0530

    fixes

--- frontend/src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-import { HTMLMotionProps } from 'framer-motion';
-
-declare module 'framer-motion' {
-  export interface MotionProps extends HTMLMotionProps<'div'> {
-    className?: string;
-  }
-}
